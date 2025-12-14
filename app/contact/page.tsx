febd23d1ae6eb0dc62e0942149c4b5abf89d4402
magodeouz <62768197+magodeouz@users.noreply.github.com>
2025-12-14 16:55:46 +0300

    chore: remove map section from contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,7 +2,6 @@ import TopHeader from "@/components/layout/header/TopHeader";
 import Header from "@/components/layout/header/Header";
 import CmnBanner from "@/components/layout/banner/CmnBanner";
 import ContactContent from "@/components/containers/contact/ContactContent";
-import ContactMap from "@/components/containers/contact/ContactMap";
 import Footer from "@/components/layout/footer/Footer";
 import CustomCursor from "@/components/layout/CustomCursor";
 import ScrollProgressButton from "@/components/layout/ScrollProgressButton";
@@ -14,7 +13,6 @@ const page = () => {
       <Header />
       <CmnBanner title="Contact Us" />
       <ContactContent />
-      <ContactMap />
       <Footer />
       <CustomCursor />
       <ScrollProgressButton />
